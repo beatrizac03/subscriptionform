76a22506686de500197d07a84d6484f892eb152c
beatrizac03 <beatrizaguiarc14@gmail.com>
2024-04-18 16:24:20 -0300

    updt 1.2

--- script.js
+++ script.js
@@ -4,19 +4,22 @@ let btnSendSubscription = document.querySelector('.sendsubscription')
 
 let tbodyCont = document.querySelector('.tbody-table')
 
+let currentIndex = 0
+
 let participants = []
 
 btnSendSubscription.addEventListener('click', sendSubscription)
 
 function sendSubscription() {
-    let name = inputName.value  
-    let email = inputEmail.value
+    let userName = inputName.value  
+    let userEmail = inputEmail.value
 
-    participants.push({name: name, email: email})
+    participants.push({name: userName, email: userEmail})
 
     inputName.value = ''
     inputEmail.value = ''
 
+    currentIndex++
     registerIntoTable()
 }
 
@@ -27,14 +30,36 @@ function registerIntoTable() {
     let eachParticipantRow = document.createElement('tr')
     eachParticipantRow.style.width = '100%'
     eachParticipantRow.style.border = '1px solid brown'
-    eachParticipantRow.style.height = '100px'
+    eachParticipantRow.style.height = '55px'
+
     
         let eachParticipantCell = document.createElement('td')
-        eachParticipantCell.textContent= `OI`
+        let eachSubscriptionDateCell = document.createElement('td')
+        let eachCheckInDateCell = document.createElement('td')
+
+        participants.forEach(participant => {
+            /*eachParticipantCell.textContent = `${participant.name} - ${participant.email}` */
+            eachParticipantCell.innerHTML = `
+            <div class="cont-row" style="display: flex; flex-direction: column; width: 100%; height: 100%">
+                <div class="p-namecont">${participant.name}</div>
+                <div class="p-emailcont">${participant.email}</div>
+            </div>`
+
+            let randomNumb = Math.floor(Math.random() * 60) + 1
+            console.log(randomNumb)
+
+            eachSubscriptionDateCell.innerHTML = `
+            <div class="cont-row" style="display: flex; flex-direction: column; width: 100%; height: 100%">
+                <div class="p-namecont"><span>há ${randomNumb} minutos</span></div>
+            </div>
+                `
+        })
+        /*  eachParticipantCell.textContent= `OI`
         eachParticipantCell.style.width = '100%'
-        eachParticipantCell.style.height = '100%'
+        eachParticipantCell.style.height = '100%' tabela */
    
     eachParticipantRow.appendChild(eachParticipantCell)
+    eachParticipantRow.appendChild(eachSubscriptionDateCell)
 
     tbodyCont.appendChild(eachParticipantRow)
 }
